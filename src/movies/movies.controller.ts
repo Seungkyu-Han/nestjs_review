28601ab5345d1c2a6a3df0a18aebe535920a78f1
Seungkyu-Han <trust1204@gmail.com>
2026-06-29 16:22:06 +0900

    feat: add movie service logic

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller, Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MoviesService, Movie } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getMovies(@Query('title') title?: string): Movie[] {
+    return this.moviesService.getManyMovies(title);
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    return this.moviesService.getMovieById(+id);
+  }
+
+  @Post()
+  postMovie(@Body() createMovieDto: CreateMovieDto) {
+    return this.moviesService.createMovie(createMovieDto.title);
+  }
+
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.moviesService.updateMovie(+id, updateMovieDto.title ?? '');
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    return this.moviesService.deleteMovie(+id);
+  }
+}
